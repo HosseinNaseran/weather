8767a77cf030a67f3e274cc28253db95bd9d4c68
HosseinNaseran <hossein.naseran@gmail.com>
2026-02-12 23:21:57 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+const cities = [
+  { id: 1, name: "Tehran", lat: "35.7219", lon: "51.3347" },
+  { id: 2, name: "ahvaz", lat: "31.3183", lon: "48.6706" },
+  { id: 3, name: "yasd", lat: "31.8974", lon: "54.3569" },
+];
+
+function App() {
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const {value} = e.target;
+    console.log(JSON.parse(value))
+  };
+
+  return (
+    <>
+      <h1>weather app</h1>
+
+      <select onChange={handleChange()}>
+        {cities.map((item) => (
+          <option key={item.id} value={JSON.stringify(item)}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+}
+
+export default App;
