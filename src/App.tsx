56670828fe2a395310e1d451ffd6fb944a9ca1a2
HosseinNaseran <hossein.naseran@gmail.com>
2026-02-14 18:11:56 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,37 @@ import { useState } from "react";
 import "./App.css";
 import { getCurrentWeather } from "./services/api";
 import type { WeatherData } from "./types/server";
-
-interface locationType {
-  id: number;
-  name: string;
-  lat: string;
-  lon: string;
-}
-
-const cities = [
-  { id: 1, name: "Tehran", lat: "35.7219", lon: "51.3347" },
-  { id: 2, name: "ahvaz", lat: "31.3183", lon: "48.6706" },
-  { id: 3, name: "yasd", lat: "31.8974", lon: "54.3569" },
-];
+import type { LocationType } from "./types/city";
+import { cities } from "./data/cities";
 
 function App() {
   const [weatherData, setWeatherData] = useState<WeatherData>();
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { value } = e.target;
-    const location: locationType = JSON.parse(value);
+    const location: LocationType = JSON.parse(value);
     getCurrentWeather({ lat: location.lat, lon: location.lon }).then(
       (result) => {
         setWeatherData(result);
+        console.log(result);
+
       },
     );
   };
+  const [isPersian, setIsPersian] = useState(false);
 
   return (
     <>
-      <h1>weather app</h1>
+      {isPersian ?
+        <button onClick={() => setIsPersian(false)}>English</button>
+        :
+        <button onClick={() => setIsPersian(true)}>فارسی</button>
+      }
+     {isPersian ? <h1>هواشناسی</h1> : <h1>weather app</h1>}
 
-      <h2>presure : {weatherData?.main.pressure}</h2>
-      <h2> temp :{weatherData?.main.temp}</h2>
-      <h2>wind speed :{weatherData?.wind.speed}</h2>
+      {isPersian ?   <h2>فشار هوا : {weatherData?.main.pressure}</h2>:<h2>presure : {weatherData?.main.pressure}</h2>}
+      {isPersian ?  <h2>دمای هوا :{weatherData?.main.temp}</h2>:<h2> temp :{weatherData?.main.temp}</h2> }
+      {isPersian ? <h2>سرعت باد :{weatherData?.wind.speed}</h2> : <h2>wind speed :{weatherData?.wind.speed}</h2>}
 
       <select onChange={handleChange}>
         {cities.map((item) => (
---
+++ src/data/cities.ts
@@ -0,0 +1,6 @@
+export const cities = [
+  { id: 1, name: "Tehran", lat: "35.7219", lon: "51.3347" },
+  { id: 2, name: "ahvaz", lat: "31.3183", lon: "48.6706" },
+  { id: 3, name: "yasd", lat: "31.8974", lon: "54.3569" },
+  {id : 4 , name : "Tabriz" , lat:"38.0814" , lon : "46.2919"},
+];
