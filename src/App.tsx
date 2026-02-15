5df56606a424aeeaf1a99035a6927e89fb082c36
HosseinNaseran <hossein.naseran@gmail.com>
2026-02-16 02:29:09 +0330

    update live css

--- src/App.tsx
+++ src/App.tsx
@@ -22,49 +22,52 @@ function App() {
   const [isPersian, setIsPersian] = useState(false);
   const temp = weatherData?.main.temp ? weatherData.main.temp - 273.15 : undefined;
   return (
-    <div className="weather-items-container">
-     
-     
-     {isPersian ? <h1>هواشناسی</h1> : <h1>weather app</h1>}
-    
-    
-      <div className="weather-items">
+    <div className="container">
 
-      <div className="weather-item">
+      <span className="shape"></span>
+      <div className="weather-items-container">
 
-        {isPersian ? <div><h2>فشار هوا   :</h2><span> {weatherData?.main.pressure}</span> </div>:
-        <div> <h2>presure :</h2> <span>{weatherData?.main.pressure}</span> </div>}
+        {isPersian ? <h1>هواشناسی</h1> : <h1>weather app</h1>}
+
+
+        <div className="weather-items">
+
+          <div className="weather-item">
+
+            {isPersian ? <div><h2>فشار هوا   :</h2><span> {weatherData?.main.pressure}</span> </div> :
+              <div> <h2>presure :</h2> <span>{weatherData?.main.pressure}</span> </div>}
+          </div>
+          <div className="weather-item">
+
+            {isPersian ? <div><h2>دمای هوا : </h2> <span>{temp?.toFixed()}°C</span></div>
+              : <div><h2> temp : </h2><span>{temp?.toFixed()}°C</span></div>}
+          </div>
+          <div className="weather-item">
+
+            {isPersian ? <div><h2>سرعت باد :</h2> <span>{weatherData?.wind.speed}</span></div>
+              : <div><h2>wind speed :</h2><span>{weatherData?.wind.speed}</span></div>}
+          </div>
+
+        </div>
+
+        <div className="weather-buttons">
+
+          {isPersian ?
+            <button onClick={() => setIsPersian(false)}>English</button>
+            :
+            <button onClick={() => setIsPersian(true)}>فارسی</button>
+          }
+
+          <select onChange={handleChange}>
+            {cities.map((item) => (
+              <option key={item.id} value={JSON.stringify(item)}>
+                {isPersian ? item.Iname : item.Ename}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
-      <div className="weather-item">
-
-        {isPersian ? <div><h2>دمای هوا : </h2> <span>{temp?.toFixed()}°C</span></div>
-        : <div><h2> temp : </h2><span>{temp?.toFixed()}°C</span></div>}
-      </div>
-      <div className="weather-item">
-
-        {isPersian ? <div><h2>سرعت باد :</h2> <span>{weatherData?.wind.speed}</span></div>
-        : <div><h2>wind speed :</h2><span>{weatherData?.wind.speed}</span></div>}
-      </div>
-
-      </div>
-
-      <div className="weather-buttons">
-
- {isPersian ?
-        <button onClick={() => setIsPersian(false)}>English</button>
-        :
-        <button onClick={() => setIsPersian(true)}>فارسی</button>
-      }
-
-      <select onChange={handleChange}>
-        {cities.map((item) => (
-          <option key={item.id} value={JSON.stringify(item)}>
-            {isPersian ? item.Iname : item.Ename}
-          </option>
-        ))}
-      </select>
     </div>
-      </div>
   );
 }
 
