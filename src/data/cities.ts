863deea73009457e91aa2baeb36b602df0dd0d12
HosseinNaseran <hossein.naseran@gmail.com>
2026-02-16 02:44:46 +0330

    API Update

--- src/data/cities.ts
+++ src/data/cities.ts
@@ -1,6 +1,12 @@
 export const cities = [
-  { id: 1, Ename: "Tehran" , Iname :"تهران", lat: "35.7219", lon: "51.3347" },
-  { id: 2, Ename: "Ahvaz", Iname :"اهواز", lat: "31.3183", lon: "48.6706" },
-  { id: 3, Ename: "Yasd", Iname :"یزد", lat: "31.8974", lon: "54.3569" },
-  {id : 4 , Ename : "Tabriz", Iname : "تبریز" , lat:"38.0814" , lon : "46.2919"},
+  { id: 1, Ename: "Tehran", Iname: "تهران", lat: "35.7219", lon: "51.3347" },
+  { id: 2, Ename: "Ahvaz", Iname: "اهواز", lat: "31.3183", lon: "48.6706" },
+  { id: 3, Ename: "Yazd", Iname: "یزد", lat: "31.8974", lon: "54.3569" },
+  { id: 4, Ename: "Tabriz", Iname: "تبریز", lat: "38.0814", lon: "46.2919" },
+  { id: 5, Ename: "Isfahan", Iname: "اصفهان", lat: "32.6546", lon: "51.6678" },
+  { id: 6, Ename: "Shiraz", Iname: "شیراز", lat: "29.6051", lon: "52.5317" },
+  { id: 7, Ename: "Kashan", Iname: "کاشان", lat: "33.9822", lon: "51.4000" },
+  { id: 8, Ename: "Mashhad", Iname: "مشهد", lat: "36.2342", lon: "59.6070" },
+  { id: 9, Ename: "Rasht", Iname: "رشت", lat: "37.2800", lon: "49.5800" },
+  { id: 10, Ename: "Qom", Iname: "قم", lat: "34.6400", lon: "50.8700" },
 ];
