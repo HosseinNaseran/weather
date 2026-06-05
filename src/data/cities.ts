4b04ded5c8c825611ddc90ce7e5c747b0b9d1fdd
HosseinNaseran <hossein.naseran@gmail.com>
2026-06-05 17:45:18 +0330

    new cities added

--- src/data/cities.ts
+++ src/data/cities.ts
@@ -1,7 +1,6 @@
 
-
 export const cities = [
-  { id: 1, Ename: "Tehran", Iname: "تهران", lat: "35.7219", lon: "51.3347", },
+  { id: 1, Ename: "Tehran", Iname: "تهران", lat: "35.7219", lon: "51.3347" },
   { id: 2, Ename: "Ahvaz", Iname: "اهواز", lat: "31.3183", lon: "48.6706" },
   { id: 3, Ename: "Yazd", Iname: "یزد", lat: "31.8974", lon: "54.3569" },
   { id: 4, Ename: "Tabriz", Iname: "تبریز", lat: "38.0814", lon: "46.2919" },
@@ -11,4 +10,9 @@ export const cities = [
   { id: 8, Ename: "Mashhad", Iname: "مشهد", lat: "36.2342", lon: "59.6070" },
   { id: 9, Ename: "Rasht", Iname: "رشت", lat: "37.2800", lon: "49.5800" },
   { id: 10, Ename: "Qom", Iname: "قم", lat: "34.6400", lon: "50.8700" },
+  { id: 11, Ename: "Karaj", Iname: "کرج", lat: "35.8400", lon: "50.9391" },
+  { id: 12, Ename: "Urmia", Iname: "ارومیه", lat: "37.5553", lon: "45.0725" },
+  { id: 13, Ename: "Kermanshah", Iname: "کرمانشاه", lat: "34.3277", lon: "47.0778" },
+  { id: 14, Ename: "Sari", Iname: "ساری", lat: "36.5633", lon: "53.0601" },
+  { id: 15, Ename: "Bandar Abbas", Iname: "بندرعباس", lat: "27.1832", lon: "56.2666" },
 ];
